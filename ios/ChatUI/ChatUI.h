0fffe1fd679e3d43ead25bab8b9a3fff189e1b3c
850853654@qq.com <850853654@qq.com>
2019-05-12 23:23:06 +0800

    修改格式

---
+++ ios/ChatUI/ChatUI.h
@@ -0,0 +1,19 @@
+//
+//  ChatUI.h
+//  ChatUI
+//
+//  Created by zhao on 2019/5/12.
+//  Copyright © 2019 zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ChatUI.
+FOUNDATION_EXPORT double ChatUIVersionNumber;
+
+//! Project version string for ChatUI.
+FOUNDATION_EXPORT const unsigned char ChatUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChatUI/PublicHeader.h>
+
+
